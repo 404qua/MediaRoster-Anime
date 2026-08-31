bbcb0fd1a78d36734970d002bda9940383d99b82
aqua <shadowninjako500@gmail.com>
2026-09-01 01:25:35 +0600

    temp build commit

---
+++ js/supabase.73241ba2.js
@@ -0,0 +1,1 @@
+import{createClient}from"https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";export const SUPABASE_URL="https://fapruzhgfktbbdygerko.supabase.co";export const SUPABASE_PUBLISHABLE_KEY="sb_publishable_ggWungI3Qt4RaCwgI_tEdw_z3z6w9fC";export const supabase=createClient(SUPABASE_URL,SUPABASE_PUBLISHABLE_KEY);
